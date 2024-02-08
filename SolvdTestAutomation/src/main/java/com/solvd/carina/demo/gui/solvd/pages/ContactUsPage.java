9047c6fdb74079cec19fe1ccd5b51b9e32c52324
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-02-08 03:55:35 -0300

    test on ContactUsPage done

--- SolvdTestAutomation/src/main/java/com/solvd/carina/demo/gui/solvd/pages/ContactUsPage.java
+++ SolvdTestAutomation/src/main/java/com/solvd/carina/demo/gui/solvd/pages/ContactUsPage.java
@@ -8,10 +8,56 @@ import org.openqa.selenium.support.FindBy;
 public class ContactUsPage extends AbstractPage {
 
     @FindBy(xpath = "//*[@aria-label='Scroll to contact form']")
-    private ExtendedWebElement ScrollToContactFormButton;
+    private ExtendedWebElement getInTouchWithUsButton;
+
+    @FindBy(xpath = "//*[@aria-label='Full name']")
+    private ExtendedWebElement fullNameInputField;
+
+    @FindBy(xpath = "//*[@aria-label='Email']")
+    private ExtendedWebElement emailInputField;
+
+    @FindBy(xpath = "//*[@aria-label='Phone number']")
+    private ExtendedWebElement phoneInputField;
+
+    @FindBy(xpath = "//*[@aria-label='Message']")
+    private ExtendedWebElement messageInputField;
+
+    @FindBy(xpath = "//*[@name='checkboxPrivacy']")
+    private ExtendedWebElement privacyPolicyCheckBox;
+
+    @FindBy(xpath = "//*[@aria-label='Submit the form']")
+    private ExtendedWebElement sendFormButton;
 
     public ContactUsPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void clickGetInTouchWithUsButton () {
+        getInTouchWithUsButton.click();
+    }
+
+    public void typeFullName (String fullName) {
+        fullNameInputField.type(fullName);
+    }
+
+    public void typeEmail (String email) {
+        emailInputField.type(email);
+    }
+
+    public void typePhone (String phone) {
+        phoneInputField.type(phone);
+    }
+
+    public void typeMessage (String message) {
+        messageInputField.type(message);
+    }
+
+    public void clickPrivacyPolicyCheckBox () {
+        privacyPolicyCheckBox.click();
+    }
+
+    public void clickSendFormButton () {
+        sendFormButton.click();
     }
 
 
--- SolvdTestAutomation/src/test/java/com/solvd/carina/demo/WebSolvdTest.java
+++ SolvdTestAutomation/src/test/java/com/solvd/carina/demo/WebSolvdTest.java
@@ -1,5 +1,6 @@
 package com.solvd.carina.demo;
 
+import com.solvd.carina.demo.gui.solvd.pages.ContactUsPage;
 import com.solvd.carina.demo.gui.solvd.pages.HomePage;
 import com.zebrunner.carina.core.IAbstractTest;
 import org.testng.annotations.Test;
@@ -9,8 +10,20 @@ public class WebSolvdTest implements IAbstractTest {
     @Test
     public void contactUsTest() {
         HomePage homePage = new HomePage(getDriver());
-        homePage.open();
+        ContactUsPage contactUsPage = new ContactUsPage(getDriver());
 
+        homePage.open();
         homePage.clickContactUsButton();
+
+        contactUsPage.clickGetInTouchWithUsButton();
+        contactUsPage.typeFullName("Manuel Miguez");
+        contactUsPage.typeEmail("manu@gmail.com");
+        contactUsPage.typePhone("+54 11987654321");
+        contactUsPage.typeMessage("I want to talk to Ivan");
+        contactUsPage.clickPrivacyPolicyCheckBox();
+//        commented part of test to not send anything
+//        contactUsPage.clickSendFormButton();
+
+
     }
 }
