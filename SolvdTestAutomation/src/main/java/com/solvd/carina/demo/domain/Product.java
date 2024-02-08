b0c89e3fb8c6af9054e52e063b5ee94373018824
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-02-07 23:11:41 -0300

    Homework API testing done

---
+++ SolvdTestAutomation/src/main/java/com/solvd/carina/demo/domain/Product.java
@@ -0,0 +1,50 @@
+package com.solvd.carina.demo.domain;
+
+public class Product {
+
+    private String id;
+    private String title;
+    private String description;
+    private String brand;
+    private String category;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
---
+++ SolvdTestAutomation/src/test/java/com/solvd/carina/demo/ProductTest.java
@@ -0,0 +1,52 @@
+package com.solvd.carina.demo;
+
+import com.solvd.carina.demo.api.DeleteProductById;
+import com.solvd.carina.demo.api.GetProductById;
+import com.solvd.carina.demo.api.PostProduct;
+import com.solvd.carina.demo.api.PutProduct;
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.testng.annotations.Test;
+
+
+public class ProductTest implements IAbstractTest {
+
+    @Test
+    @MethodOwner(owner = "manumiguezz")
+    public void getProductByIdTest(){
+        GetProductById api = new GetProductById();
+        api.setId(1);
+        api.callAPIExpectSuccess();
+        api.validateResponseAgainstSchema("api/products/_get/rs.json");
+    }
+
+    @Test
+    @MethodOwner(owner = "manumiguezz")
+    public void deleteProductTest(){
+        DeleteProductById api = new DeleteProductById();
+        api.setId(1);
+        api.callAPIExpectSuccess();
+        api.validateResponse(JSONCompareMode.STRICT_ORDER);
+    }
+
+    @Test
+    @MethodOwner(owner = "manumiguezz")
+    public void postProductTest(){
+        PostProduct api = new PostProduct();
+        api.callAPIExpectSuccess();
+        api.validateResponse();
+        api.validateResponseAgainstSchema("api/products/_post/rs.json");
+    }
+
+    @Test
+    @MethodOwner(owner = "manumiguezz")
+    public void putProductTest(){
+        PutProduct api = new PutProduct();
+        api.setId(1);
+        api.callAPIExpectSuccess();
+        api.validateResponse(JSONCompareMode.STRICT_ORDER);
+        api.validateResponseAgainstSchema("api/products/_put/rs.json");
+    }
+
+}
